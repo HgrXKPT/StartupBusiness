5fa932152297b1fc82e02a1d9258428c362ce8e5
Higor <higorestevao02@hotmail.com>
2025-04-02 21:03:44 -0300

    Adicionar arquivos de projeto.

---
+++ StartupBusiness/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿namespace StartupBusiness.Models
+{
+    public class Users
+    {
+        private string _name;
+        private string _email;
+        private string _cpf;
+        private string _role;
+        private string _rh;
+        private string _senha;
+
+        public string Name {
+            get => _name;
+
+            set {
+
+            }
+        }
+
+    }
+}
