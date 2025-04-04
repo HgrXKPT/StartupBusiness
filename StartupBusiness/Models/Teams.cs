28089e33113a68cdeb85b863d15116ac0417d0ca
Higor <higorestevao02@hotmail.com>
2025-04-03 22:57:01 -0300

    User Models created

---
+++ StartupBusiness/Models/Teams.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StartupBusiness.Models
+{
+    public class Teams
+    {
+        private string _teamName;
+        private int _memberCount;
+        private int _projectCount;
+
+        [Required(ErrorMessage ="Team name Field is required")]
+        public string TeamName {
+            get => _teamName;
+            set;
+        }
+    }
+}
--- StartupBusiness/Models/Users.cs
+++ StartupBusiness/Models/Users.cs
@@ -1,21 +1,47 @@
-﻿namespace StartupBusiness.Models
+﻿using StartupBusiness.Utils;
+using System.ComponentModel.DataAnnotations;
+
+namespace StartupBusiness.Models
 {
     public class Users
     {
         private string _name;
         private string _email;
-        private string _cpf;
-        private string _role;
-        private string _rh;
-        private string _senha;
+        private string _password;
 
+        [Required(ErrorMessage ="Name field is requried")]
         public string Name {
             get => _name;
 
             set {
-
+                _name = value.Trim();
             }
         }
+
+        [Required(ErrorMessage ="Email field is required")]
+        [EmailAddress(ErrorMessage = "The email provided is invalid")]
+        public string Email {
+            get => _email;
+
+            set {
+
+                
+                    _email = value;
+                
+            }
+
+        }
+
+        [Required(ErrorMessage ="Password field is required")]
+        public string Password {
+            get => _password;
+            set {
+
+                _password = value;
+            }
+        }
+
+        
 
     }
 }
