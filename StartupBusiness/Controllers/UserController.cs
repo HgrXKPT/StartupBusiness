bdfc0075a83afa9ffe2e5ba2a7b6a585504c0cf7
Higor <higorestevao02@hotmail.com>
2025-04-05 18:01:56 -0300

    initialized the creation of controllers

---
+++ StartupBusiness/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StartupBusiness.Data;
+using StartupBusiness.Models;
+
+namespace StartupBusiness.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController: ControllerBase
+    {
+
+
+
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            //Provisorio para não ocorrer mais erros durante a criação
+            await Task.Delay(100);
+
+            return Ok(new { mensagem = "Usuario criado com sucesso" } );
+
+        }
+       
+
+    }
+}
